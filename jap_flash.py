fab11cd8049539067d02c916d09faa1bc4e9174c
115GOTYOUR6 <jay.oliver2016@outlook.com>
2021-04-24 11:10:42 +0800

    Feature addition and reformat of jap_words.csv
    An additional command line feature was added to allow what is being
    tested for each word to be specified. The hiragana of a word featuring
    kanji will now be displayed when passing on a word.
    
    The rework of the csv file merely involves removing the word type from
    the list of word parameters, felt it was unecessary and would merely
    slow the addition of other words.

--- jap_flash.py
+++ jap_flash.py
@@ -11,6 +11,10 @@ parser.add_argument("filepath", type=str, help="the filepath to the text file"
                                                " containing all the words to"
                                                " be featured in the test")
 
+parser.add_argument("test", type=str, choices=["meaning", "hiragana"],
+                    help="Determines the information the user will have to"
+                         " provide for the words presented.")
+
 args = parser.parse_args()
 
 words = []
@@ -18,25 +22,28 @@ with open(args.filepath, newline='') as f:
     reader = csv.DictReader(f)
     for row in reader:
         row["meaning"] = row["meaning"].split('/')
+        row["hiragana"] = row["hiragana"].split('/')
         words.append(row)
 
 print("Type the answer or q to quit")
 print("If you can't answer a question type p to pass")
 print()
 i = -1
-test = "word"
+info = "word"
 while i != 'q':
     i = -1
     word_ind = random.randint(0, len(words)-1)
-    print(words[word_ind][test])
-    while i not in words[word_ind]["meaning"] and i != 'p' and i != 'q':
+    print(words[word_ind][info])
+    while i not in words[word_ind][args.test] and i != 'p' and i != 'q':
         i = input()
         if i == 'p':
-            print("The answer was: {}".format(words[word_ind]["meaning"]))
+            print("The answer was: {}".format(words[word_ind][args.test]))
+            if args.test == "meaning" and words[word_ind]["kanji"] == 'y':
+                print("Hiragana: {}".format(words[word_ind]["hiragana"]))
         if i == 'q':
             pass
-        if i in words[word_ind]["meaning"]:
+        if i in words[word_ind][args.test]:
             print("Correct")
-            if len(words[word_ind]["meaning"]) > 1:
+            if len(words[word_ind][args.test]) > 1:
                 print("All meanings are: "
-                      "{}".format(words[word_ind]["meaning"]))
+                      "{}".format(words[word_ind][args.test]))
