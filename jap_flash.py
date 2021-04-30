ee27bc6eb1cb0f8243267a668e1f5daedca01258
115GOTYOUR6 <jay.oliver2016@outlook.com>
2021-04-30 10:15:57 +0800

    Feature addition and more info for answers
    A new command line argument has been added that controls the featuring
    of words once or infinite times in the test. This was added because the
    random number generator sucks.
    
    The hiragana of words is also printed to terminal when answering the
    meaning of a word correctly.

--- jap_flash.py
+++ jap_flash.py
@@ -16,6 +16,11 @@ parser.add_argument("test", type=str, choices=["meaning", "hiragana"],
                     help="Determines the information the user will have to"
                          " provide for the words presented.")
 
+parser.add_argument("mode", type=str, choices=["single", "repeated"],
+                    help="Changes the test to either only show each word once"
+                         " throughout the test 'single' or to repeat any of"
+                         " the words infinitely")
+
 args = parser.parse_args()
 
 words = []
@@ -42,18 +47,26 @@ i = -1
 info = "word"
 while i != 'q':
     i = -1
-    word_ind = random.randint(0, len(words)-1)
-    print(words[word_ind][info])
-    while i not in words[word_ind][args.test] and i != 'p' and i != 'q':
-        i = input()
-        if i == 'p':
-            print("The answer was: {}".format(words[word_ind][args.test]))
-            if args.test == "meaning" and words[word_ind]["kanji"] == 'y':
-                print("Hiragana: {}".format(words[word_ind]["hiragana"]))
-        if i == 'q':
-            pass
-        if i in words[word_ind][args.test]:
-            print("Correct")
-            if len(words[word_ind][args.test]) > 1:
-                print("All meanings are: "
-                      "{}".format(words[word_ind][args.test]))
+    if len(words) == 0:
+        print("No words are left")
+        i = 'q'
+    else:
+        word_ind = random.randint(0, len(words)-1)
+        print(words[word_ind][info])
+        while i not in words[word_ind][args.test] and i != 'p' and i != 'q':
+            i = input()
+            if i == 'p':
+                print("The answer was: {}".format(words[word_ind][args.test]))
+                if args.test == "meaning" and words[word_ind]["kanji"] == 'y':
+                    print("Hiragana: {}".format(words[word_ind]["hiragana"]))
+            if i == 'q':
+                pass
+            if i in words[word_ind][args.test]:
+                print("Correct")
+                if len(words[word_ind][args.test]) > 1:
+                    print("All meanings are: "
+                          "{}".format(words[word_ind][args.test]))
+                if args.test == "meaning":
+                    print("hiragana: {}".format(words[word_ind]["hiragana"]))
+        if args.mode == 'single':
+            del words[word_ind]
